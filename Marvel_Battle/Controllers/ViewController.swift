26d83ad9b3a9fb922d027d6f115faae35ad0d80f
Eduardo <eduardojordan@hotmail.com>
2020-05-09 12:27:20 +0200

    Initial Git - Add gitignore, adjust App delegate, folders

---
+++ Marvel_Battle/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel_Battle
+//
+//  Created by MAC on 09/05/2020.
+//  Copyright © 2020 eduardojordan.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Marvel_BattleTests/Marvel_BattleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Marvel_BattleTests.swift
+//  Marvel_BattleTests
+//
+//  Created by MAC on 09/05/2020.
+//  Copyright © 2020 eduardojordan.com. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Battle
+
+class Marvel_BattleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
